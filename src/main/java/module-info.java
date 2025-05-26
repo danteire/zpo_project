6f5cc52124175565a3c31ff53d991d99953c97e2
Iredion <246360@edu.p.lodz.pl>
2025-05-26 21:14:41 +0200

    App View Added

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module listaobecnosci {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens org.example.listaobecnosci;
+}
--- src/main/java/org/example/listaobecnosci/ListaobecnosciApplication.java
+++ src/main/java/org/example/listaobecnosci/ListaobecnosciApplication.java
@@ -8,6 +8,9 @@ public class ListaobecnosciApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ListaobecnosciApplication.class, args);
+
     }
+
+
 
 }
